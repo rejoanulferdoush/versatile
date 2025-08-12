1c5701abc51653442f213ee3b9b9b836c8a2e962
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-13 00:24:33 +0600

    nav section is ongoing without responsive

---
+++ src/routes/components/hero.svelte
@@ -0,0 +1,15 @@
+<!-- header -->
+ <header class="padding__xxl margin__top--base">
+    <!-- nav -->
+    <nav class="nav__container">
+        <div class="logo">Fitness</div>
+        <div class="nav__menu--big">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+    </nav>
+ </header>
