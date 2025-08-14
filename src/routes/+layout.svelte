547d358fa12f436c0b94f68934041f91dbe974ec
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-15 00:52:41 +0600

    nav portion is done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import "../app.css";
+	import "../media.css"
 
 	let { children } = $props();
 </script>
--- src/routes/components/hero.svelte
+++ src/routes/components/hero.svelte
@@ -1,15 +1,27 @@
 <!-- header -->
- <header class="padding__xxl margin__top--base">
-    <!-- nav -->
-    <nav class="nav__container">
-        <div class="logo">Fitness</div>
-        <div class="nav__menu--big">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Service</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-        </div>
-    </nav>
- </header>
+<header class="padding__xxl margin__top--base">
+  <!-- nav -->
+  <nav class="nav__container">
+    <div class="logo">Fitness</div>
+    <!-- menu big -->
+    <div class="nav__menu--big">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+    <!-- menu small -->
+    <div class="nav__menu--small">
+      <i class="fa-solid fa-bars"></i>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
+
