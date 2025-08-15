1b7bd7201db9c16f6c4fd38a7c46beff7141292d
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-16 00:56:48 +0600

    heros section > nav and banner is done

--- src/routes/components/hero.svelte
+++ src/routes/components/hero.svelte
@@ -1,5 +1,5 @@
 <!-- header -->
-<header class="padding__xxl margin__top--base">
+<header class="padding__xxl padding__top--base">
   <!-- nav -->
   <nav class="nav__container">
     <div class="logo">Fitness</div>
@@ -23,5 +23,30 @@
       </ul>
     </div>
   </nav>
+  <!-- banner -->
+  <div class="banner__container">
+    <!-- banner left -->
+    <div class="left">
+      <h1 class="section__text--xxl">Get body in <span class="special__text">shape</span> & stay healthy</h1>
+      <p class="primary__text margin__top--sm">A huge selection of health and fitness content, healthy recipes and transformation stories to help you get fit and stay fit!</p>
+    </div>
+    <!-- banner right -->
+     <div class="banner__right">
+      <div class="banner__image">
+        <img src="/img/Images2.png" alt="" srcset="">
+      </div>
+      <div class="tutorial">
+        <a class="left__tutorial" href="#"><i class="fa-solid fa-play"></i></a>
+        <div class="right__tutorial">
+          <h4 class="section__text--xl">350+</h4>
+          <p class="primary__text">Video Tutorial</p>
+        </div>
+      </div>
+      <div class="workout">
+          <h4 class="section__text--xl">500+</h4>
+          <p class="primary__text">Free Workout videos</p>
+      </div>
+     </div>
+  </div>
 </header>
 
