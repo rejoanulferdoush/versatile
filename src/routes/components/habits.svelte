11a26debf9cc028792053a08927aa5db651b66a5
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-17 00:48:42 +0600

    habits section is done

---
+++ src/routes/components/habits.svelte
@@ -0,0 +1,44 @@
+<section class="habits__container padding__xxl margin__top--xxl">
+    <div class="habits__header">
+        <h2 class="section__text--xl">Change Your Habits</h2>
+        <p class="primary__text margin__top--sm">We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym.</p>
+    </div>
+    <div class="habits__card--container margin__top--base">
+        <div class="movement">
+            <div class="movement__img">
+                <img src="../img/image3.png" alt="" srcset="">
+            </div>
+            <div class="movement__content">
+                <h4>Movement</h4>
+                <p class="primary__text">We believe fitness should be accessible to everyone</p>
+            </div>
+        </div>
+        <div class="practice">
+            <div class="practice__img">
+                <img src="../img/image4.png" alt="" srcset="">
+            </div>
+            <div class="practice__content">
+                <h4>Practice</h4>
+                <p class="primary__text">We believe fitness should be accessible to everyone</p>
+            </div>
+        </div>
+        <div class="time">
+            <div class="time__img">
+                <img src="../img/image2.png" alt="" srcset="">
+            </div>
+            <div class="time__content">
+                <h4>Time</h4>
+                <p class="primary__text">We believe fitness should be accessible to everyone</p>
+            </div>
+        </div>
+        <div class="weight">
+            <div class="weight__img">
+                <img src="../img/image3.png" alt="" srcset="">
+            </div>
+            <div class="weight__content">
+                <h4>Weight Loss</h4>
+                <p class="primary__text">We believe fitness should be accessible to everyone</p>
+            </div>
+        </div>
+    </div>
+</section>
