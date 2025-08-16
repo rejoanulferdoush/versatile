92ddae44c624a902b4019a7266623735538fc651
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-17 02:00:15 +0600

    run extra section is done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import Hero from "./components/hero.svelte";
     import Habits from "./components/habits.svelte";
+    import Extra from "./components/extra.svelte";
 </script>
 
 <Hero 
 hero__desc = "A huge selection of health and fitness content, healthy recipes and transformation stories to help you get fit and stay fit!";
 />
 <Habits />
+<Extra />
---
+++ src/routes/components/extra.svelte
@@ -0,0 +1,16 @@
+<section class="margin__top--xxl padding__xxl">
+  <div class="run__content--container">
+    <div class="run__left">
+      <h3 class="section__text--xxl">Run an Extra Mile Easily</h3>
+      <p class="primary__text margin__top--sm">
+        We believe fitness should be accessible to everyone, everywhere,
+        regardless of income or access to a gym. With hundreds of professional
+        workouts.
+      </p>
+      <button class="btn__primary margin__top--sm">Join Now</button>
+    </div>
+    <div class="run__image">
+      <img src="../img/run.jpg" alt="" srcset="" />
+    </div>
+  </div>
+</section>
