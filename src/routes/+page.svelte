0e7aacb4fffcfaa3fe7c6808f05b1ebf949dcb15
rejoanulferdoush <rejoanulferdoush@gmail.com>
2025-08-23 01:25:05 +0600

    meet our team with responsive is done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import Extra from "./components/extra.svelte";
     import Training from "./components/training.svelte";
     import Bmi from "./components/bmi.svelte";
+    import Team from "./components/team.svelte";
 </script>
 
 <Hero 
@@ -13,3 +14,4 @@ hero__desc = "A huge selection of health and fitness content, healthy recipes an
 <Extra />
 <Training />
 <Bmi />
+<Team />
